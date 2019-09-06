ef20f116abc9e29285a5e91e374ac6db5fd0fcc7
Tyc <tyc1239@163.com>
2019-09-06 19:27:20 +0800

    tyc-dev 博客列表

--- resources/views/admin/blogs/blogslist.blade.php
+++ resources/views/admin/blogs/blogslist.blade.php
@@ -3,48 +3,14 @@
 @section('content')
 
 <div class="row">
-    <div class="col-sm-12">
+    <div class="col-sm-15">
         <div class="ibox float-e-margins">
             <div class="ibox-title">
-                <h5>自定义响应式表格</h5>
-                <div class="ibox-tools">
-                    <a class="collapse-link">
-                        <i class="fa fa-chevron-up"></i>
-                    </a>
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="table_basic.html#">
-                        <i class="fa fa-wrench"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="table_basic.html#">选项1</a>
-                        </li>
-                        <li><a href="table_basic.html#">选项2</a>
-                        </li>
-                    </ul>
-                    <a class="close-link">
-                        <i class="fa fa-times"></i>
-                    </a>
-                </div>
+                <h5>blog列表管理</h5>
+                 
             </div>
             <div class="ibox-content">
                 <div class="row">
-                    <div class="col-sm-5 m-b-xs">
-                        <select class="input-sm form-control input-s-sm inline">
-                            <option value="0">请选择</option>
-                            <option value="1">选项1</option>
-                            <option value="2">选项2</option>
-                            <option value="3">选项3</option>
-                        </select>
-                    </div>
-                    <div class="col-sm-4 m-b-xs">
-                        <div data-toggle="buttons" class="btn-group">
-                            <label class="btn btn-sm btn-white">
-                                <input type="radio" id="option1" name="options">天</label>
-                            <label class="btn btn-sm btn-white active">
-                                <input type="radio" id="option2" name="options">周</label>
-                            <label class="btn btn-sm btn-white">
-                                <input type="radio" id="option3" name="options">月</label>
-                        </div>
-                    </div>
                     <div class="col-sm-3">
                         <div class="input-group">
                             <input type="text" placeholder="请输入关键词" class="input-sm form-control"> <span class="input-group-btn">
@@ -56,66 +22,35 @@
                     <table class="table table-striped">
                         <thead>
                             <tr>
-
-                                <th></th>
-                                <th>项目</th>
-                                <th>进度</th>
-                                <th>任务</th>
-                                <th>日期</th>
+                                <th>ID</th>
+                                <th>博客标题</th>
+                                <th width="10%"  >博客内容</th>
+                                 <th>博客图片</th>
+                                <th>添加时间</th>
+                                <th>浏览量</th>
+                                <th>用户id</th>
+                                <th>分类id</th>
                                 <th>操作</th>
                             </tr>
                         </thead>
                         <tbody>
+                          @foreach($data as $key=>$val)
                             <tr>
-                                <td>
-                                    <input type="checkbox" checked class="i-checks" name="input[]">
-                                </td>
-                                <td>米莫说｜MiMO Show</td>
-                                <td><span class="pie">0.52/1.561</span>
-                                </td>
-                                <td>20%</td>
-                                <td>2014.11.11</td>
-                                <td><a href="table_basic.html#"><i class="fa fa-check text-navy"></i></a>
+                                <td> {{$val->b_id}}</td>
+                                <td>{{$val->b_name}}</td>
+                                <td width="10%" >{{$val->b_desc}}</td>
+                                 <td><img src="/csdn/{{$val->b_img}}" width="100"></td>
+                                <td>{{$val->created_at}}</td>
+                                <td>{{$val->b_read}}</td>
+                                 <td>{{$val->u_id}}</td>
+                                 <td>{{$val->c_id}}</td>
+                                <td><a href="del?b_id={{$val->b_id}}"><i class="fa fa-times text-navy"></i></a>
                                 </td>
                             </tr>
-                            <tr>
-                                <td>
-                                    <input type="checkbox" class="i-checks" name="input[]">
-                                </td>
-                                <td>商家与购物用户的交互试衣应用</td>
-                                <td><span class="pie">6,9</span>
-                                </td>
-                                <td>40%</td>
-                                <td>2014.11.11</td>
-                                <td><a href="table_basic.html#"><i class="fa fa-check text-navy"></i></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <input type="checkbox" class="i-checks" name="input[]">
-                                </td>
-                                <td>天狼---智能硬件项目</td>
-                                <td><span class="pie">3,1</span>
-                                </td>
-                                <td>75%</td>
-                                <td>2014.11.11</td>
-                                <td><a href="table_basic.html#"><i class="fa fa-check text-navy"></i></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <input type="checkbox" class="i-checks" name="input[]">
-                                </td>
-                                <td>线下超市+线上商城+物流配送互联系统</td>
-                                <td><span class="pie">4,9</span>
-                                </td>
-                                <td>18%</td>
-                                <td>2014.11.11</td>
-                                <td><a href="table_basic.html#"><i class="fa fa-check text-navy"></i></a>
-                                </td>
-                            </tr>
+                       @endforeach
                         </tbody>
                     </table>
+                    <center>{{$data->appends($query)->links()}}</center>
                 </div>
 
             </div>
@@ -123,5 +58,7 @@
     </div>
 
 </div>
+  
+
 
 @endsection
